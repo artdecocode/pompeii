1c2f6dc66b8739e7cfd2e0d2f13325e6436acabf
Anton <anton@adc.sh>
2019-03-29 19:49:03 +0300

    pckg

--- build/index.js
+++ build/index.js
@@ -1,14 +1,9 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.default = pompeii;
-
 /**
  * This is the main package file.
  */
-async function pompeii() {
-  console.log('pompeii called');
+               async function pompeii() {
+  console.log('pompeii called')
 }
-//# sourceMappingURL=index.js.map
+
+
+module.exports = pompeii
